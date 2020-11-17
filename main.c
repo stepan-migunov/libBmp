a1e52b6d892ed9996f08172736a9c014936091b3
stepan-migunov <stepan-migunoff@yandex.ru>
2020-11-17 11:38:35 +0300

    initital commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "bmpDrawing.h"
+
+int main() {
+    //create("open.bmp",200,200);
+    openImage("open.bmp");
+    //drawLine(10,10,190,20,5,RED);
+    //drawLine(10,10,190,20,5,RED);
+    //drawCircle(10,10,5,RED);
+    writeFile();
+    return 0;
+}
