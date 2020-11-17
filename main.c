06ef7dbc417a0941e26e30c94d53197bdfa2230f
stepan-migunov <stepan-migunoff@yandex.ru>
2020-11-17 11:45:14 +0300

    added bmp image: image.bmp

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include "bmpDrawing.h"
 
 int main() {
-    //create("open.bmp",200,200);
-    openImage("open.bmp");
-    //drawLine(10,10,190,20,5,RED);
-    //drawLine(10,10,190,20,5,RED);
-    //drawCircle(10,10,5,RED);
+    create("open.bmp",200,200);
+    //openImage("open.bmp");
+    drawLine(10,10,190,20,5,RED);
+    drawLine(10,10,190,20,5,RED);
+    drawCircle(10,10,5,RED);
     writeFile();
     return 0;
 }
